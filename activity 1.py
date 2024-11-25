55a891102f5d7858d1a0c3c5a44353c9c78a0899
MazenMoh2011 <mazenalnagar203@gmail.com>
2024-11-25 15:23:41 +0300

    djskalDJSKLa;

--- activity 1.py
+++ activity 1.py
@@ -1,1 +1,9 @@
-print("jviolfeqngjadfnfgouraw ")
+medical_cause=input("did you have a medical cuase Y or N")
+ATTEN= int(input("enter the attendance of the student"))
+if medical_cause == "Y" :
+    print("you are allowed")
+else:
+    if ATTEN>=75:
+      print("allowed")
+    else:
+        print ("not allowed")
